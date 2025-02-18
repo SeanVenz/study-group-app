dddd54af121354ad8c305effd1b23f82ff46900c
shan <quijano.seanvenz@gmail.com>
2025-02-18 14:04:27 +0800

    build error fix

--- src/hooks/useGetGroups.ts
+++ src/hooks/useGetGroups.ts
@@ -1,11 +1,11 @@
-"use clientw";
+"use client";
 
 import { getPublicGroups } from "@/lib/actions";
 import { Group } from "@/types/types";
 import { useEffect, useState } from "react";
 import useAuth from "@/hooks/useAuth";
 
-function useGetGroups(publicGroup : boolean) {
+function useGetGroups(publicGroup: boolean) {
   const user = useAuth();
   const [groups, setGroups] = useState<Group[]>([]);
 
@@ -13,16 +13,20 @@ function useGetGroups(publicGroup : boolean) {
     const fetchGroups = async () => {
       const groupsData = await getPublicGroups();
 
-      if(publicGroup){
-        const filteredGroupData = groupsData.filter((group) => group.admin !== user?.displayName && 
-        !group.members?.includes(user?.displayName) && group.groupPrivacy !== 'Private');
-        setGroups(filteredGroupData)
-      }else{
-        const ownGroup = groupsData.filter((group) => group.admin === user?.displayName ||
-        group.members?.includes(user?.displayName));
-        setGroups(ownGroup)
+      if (publicGroup) {
+        const filteredGroupData = groupsData.filter((group) => 
+          group.admin !== user?.displayName && 
+          !group.members?.includes(user?.displayName ?? '') && 
+          group.groupPrivacy !== 'Private'
+        );
+        setGroups(filteredGroupData);
+      } else {
+        const ownGroup = groupsData.filter((group) => 
+          group.admin === user?.displayName || 
+          group.members?.includes(user?.displayName ?? '') 
+        );
+        setGroups(ownGroup);
       }
-      ;
     };
 
     fetchGroups();
